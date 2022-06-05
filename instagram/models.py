2a9c57f0fc23d1c753dccc84b84dd3a9b3606f1f
john.njau <john.njoroge@student.moringaschool.com>
2022-06-05 12:37:26 +0300

    feature: added profile, image and comment models

--- instagram/models.py
+++ instagram/models.py
@@ -4,95 +4,54 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    class Meta:
-        db_table = 'profile'
-
-    bio = models.TextField(max_length=200, null=True, blank=True, default="bio")
-    profilepic = models.ImageField(upload_to='picture/', null=True, blank=True)
     user=models.OneToOneField(User, on_delete=models.CASCADE, blank=True, related_name="profile")
+    bio = models.TextField(max_length=200, null=True, blank=True, default="bio")
+    profilepic = models.ImageField(upload_to='uploads/profilepics/', null=True, blank=True)
     followers = models.ManyToManyField(User, related_name="followers", blank=True)
     following = models.ManyToManyField(User, related_name="following", blank=True)
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-
-    def follow_user(self, follower):
-        return self.following.add(follower)
-
-    def unfollow_user(self, to_unfollow):
-        return self.following.remove(to_unfollow)
-
-    def is_following(self, checkuser):
-        return checkuser in self.following.all()
-
-    def get_number_of_followers(self):
-        if self.followers.count():
-            return self.followers.count()
-        else:
-            return 0
-
-    def get_number_of_following(self):
-        if self.following.count():
-            return self.following.count()
-        else:
-            return 0
-
-    @classmethod
-    def search_users(cls, search_term):
-        profiles = cls.objects.filter(user__username__icontains=search_term)
-        return profiles
-
+    date_created = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
-        return self.user.username
+        return self.user
     
 class Image(models.Model):
-    image=models.ImageField(upload_to='picture/', )
-    name = models.CharField(max_length=40)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="images")
-    description=models.TextField()
-    likes = models.IntegerField(default=0)
+    image=models.ImageField(upload_to='uploads/gramposts/', )
+    name = models.CharField(max_length=30)
+    caption=models.TextField()
+    uploader_profile = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
     comments= models.TextField(blank=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+        
+    def update_caption(self):
+        self.save()
+    
 
     def __str__(self):
         return self.name
+    
+    
+class Comments (models.Model):
+    comment = models.CharField(max_length=255)
+    author = models.ForeignKey('Profile',related_name='comment' , on_delete=models.CASCADE)
+    image_commented_on = models.ForeignKey('Image', on_delete=models.CASCADE, related_name='images')
+    comment_date = models.DateTimeField(auto_now_add=True)
 
-    def save_image(self):
-        self.save()
+    def __str__(self):
+        return self.comment
 
-    @classmethod
-    def delete_image_by_id(cls, id):
-        pictures = cls.objects.filter(pk=id)
-        pictures.delete()
 
-    @classmethod
-    def get_image_by_id(cls, id):
-        pictures = cls.objects.get(pk=id)
-        return pictures
-
-    @classmethod
-    def filter_by_tag(cls, tags):
-        pictures = cls.objects.filter(tags=tags)
-        return pictures
-
-    @classmethod
-    def filter_by_location(cls, location):
-        pictures = cls.objects.filter(location=location)
-        return pictures
-
-    @classmethod
-    def search_image(cls, search_term):
-        pictures = cls.objects.filter(name__icontains=search_term)
-        return pictures
-
-    @classmethod
-    def update_image(cls, id):
-        pictures=cls.objects.filter(id=id).update(id=id)
-        return pictures
-
-    @classmethod
-    def update_description(cls, id):
-        pictures = cls.objects.filter(id=id).update(id=id)
-        return pictures
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    
+    class Meta:
+        ordering = ['-id']
+        
+    def __str__(self):
+        return self.email
