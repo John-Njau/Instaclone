36a7da46db5e709039f1a6b1c42e88f75a9b5007
john.njau <john.njoroge@student.moringaschool.com>
2022-06-05 17:46:55 +0300

    feature: app navigation urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path, include, re_path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('upload/', views.upload, name='upload'),
+    path('profile/', views.profile, name='profile'),
+    path('updateprofile/', views.updateprofile, name='updateprofile'),
+    path('logout/', views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
