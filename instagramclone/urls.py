e1de7cef20e97a349d2e70d17c0cd27bb1ede26e
john.njau <john.njoroge@student.moringaschool.com>
2022-06-05 17:49:59 +0300

    feature: registration urls

--- instagramclone/urls.py
+++ instagramclone/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('', include('instagram.urls'))
+    path('', include('instagram.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), {"next_page": '/'}),
 ]
