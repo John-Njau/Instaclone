fc0b1326fbcf6affafd1179ed237aed7cc1a1a87
john.njau <john.njoroge@student.moringaschool.com>
2022-06-07 03:41:05 +0300

    feature: Added user interaction forms

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,29 +1,46 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.forms.widgets import TextInput, EmailInput
+from django.forms.widgets import TextInput, EmailInput, FileInput
 
-from .models import User, Subscriber
+from .models import Image, Profile, User
 
 class SignUpForm(UserCreationForm):
     class Meta:
        model = User
        fields = ('username', 'email', 'password1', 'password2')
        
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
        
-class SubscriptionForm(forms.ModelForm):
+class LoginForm(forms.Form):
+    username = forms.CharField( max_length=30)
+    password = forms.PasswordInput()
+    
     class Meta:
-        model = Subscriber
-        fields = ('email',)
+        fields = ('username', 'password')
         
-        widgets = {
-            'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'johndoe@ymail.com', 'id': 'email'}),
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','caption']
+        
+        widgets ={
+            'image': FileInput(attrs={'class': 'form-control'}),
+            'caption': TextInput(attrs={'class': 'form-control', 'placeholder': 'write something'}),
         }
         
 
-class LoginForm(forms.Form):
-    username = forms.CharField(label='usernameme', max_length=30)
-    password = forms.PasswordInput(label='Password')
-    
+class UpdateProfileForm(forms.ModelForm):
     class Meta:
-        fields = ['username', 'password']
+        model = Profile
+        fields = ['profilepic', 'bio']
+        
+        widgets ={
+            'profilepic': FileInput(attrs={'class': 'form-control'}),
+            'bio': TextInput(attrs={'class': 'form-control', 'placeholder': 'Update Bio'}),
+        }
